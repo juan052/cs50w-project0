c98f6a37e66f55c5624a1c9e7ceeca61c5bdb8ad
juanhuete052 <jrios9836@gmail.com>
2023-03-10 21:54:06 -0600

    Terminando project

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,8 +1,8 @@
 let elementoResultado = document.querySelector(".js-resultado");
-let botoesPadroes = document.querySelectorAll(".js-btn-padroes");
+let botoesPadroes = document.querySelectorAll(".js-btn-pad");
 let botaoResultado = document.querySelector(".js-btn-igual");
 let botaoAC = document.querySelector(".js-btn-ac");
-let botaoMaisMenos = document.querySelector(".js-btn-mais-menos");
+let botaoMaisMenos = document.querySelector(".js-btn-mas-menos");
 let botaoDeletar = document.querySelector(".js-btn-del");
 
 function adicionarElementoAoInputResultado(numeroDigitado) {
